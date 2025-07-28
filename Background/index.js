fa1b201223dc46558e9671a6a98eb559f636247e
rohitpechetti <162451762+rohitpechetti@users.noreply.github.com>
2025-07-28 12:23:08 +0530

    Add files via upload

---
+++ Background/index.js
@@ -0,0 +1,17 @@
+let container=document.getElementById("color");
+function Pink(){
+    container.style.backgroundColor="pink";
+    
+}
+function Blue(){
+    container.style.backgroundColor="blue";
+    
+}
+function Black(){
+    container.style.backgroundColor="black";
+    
+}
+function Green(){
+    container.style.backgroundColor="green";
+    
+}
